c2eb58d08fbb1d04374d22416db74578e89c0f7f
Ayaz Ali Qureshi <a.qureshi@sevensenders.com>
2022-05-05 14:58:49 +0200

    Adding Shipment & Order REST controllers

---
+++ codingchallenge/src/main/java/com/sevensenders/cpt/codingchallenge/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.sevensenders.cpt.codingchallenge.controller;
+
+import com.sevensenders.cpt.codingchallenge.model.Order;
+import com.sevensenders.cpt.codingchallenge.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(final OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/orders")
+    public List<Order> fetchOrders() {
+        return orderService.fetchOrders();
+    }
+
+}
