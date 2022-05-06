ecf8dba5d370869791249c55140b54a2a315e405
Yekta <s.yektamaram@sevensenders.com>
2022-05-06 12:22:49 +0200

    Added orderId for shipment entity

--- codingchallenge/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO shipment (tracking_code, warehouse_address, shipment_status, weight , created_at, updated_at) VALUES ('TRCK0001', 'BERLIN, DE', 'SHIPPED', 2,  NOW(), NOW());
-INSERT INTO SHIPMENT (tracking_code, warehouse_address, shipment_status, weight , created_at, updated_at) VALUES ('TRCK0002', 'BERLIN, DE', 'CREATED', 1, NOW(), NOW());
-INSERT INTO SHIPMENT (tracking_code, warehouse_address, shipment_status, weight , created_at, updated_at) VALUES ('TRCK0003', 'BERLIN, DE', 'CREATED', 1, NOW(), NOW());
-INSERT INTO order_entity (shipment_id, created_at, updated_at) VALUES (1, NOW(), NOW());
--- src/main/java/com/sevensenders/cpt/codingchallenge/model/Order.java
+++ src/main/java/com/sevensenders/cpt/codingchallenge/model/Order.java
@@ -1,31 +1,23 @@
 package com.sevensenders.cpt.codingchallenge.model;
 
+
 import lombok.Data;
 
-
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import javax.persistence.Entity;
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.time.Instant;
 
 @Data
-@Entity
-public class Shipment implements Serializable {
+@Entity(name = "OrderEntity")
+public class Order implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer shipmentId;
-
-    private String trackingCode;
-
-    private String warehouseAddress;
-
-    private String shipmentStatus;
-
-    private BigDecimal weight;
+    private Integer orderId;
 
     private Instant createdAt;
 
